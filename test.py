5e389bb4d49f5200b6c4f544a7b96f9a4b2e8406
sheepish_towns <yangtianshuo2009@163.com>
2022-10-08 11:32:09 -0600

    fixed jumping collision bug

--- test.py
+++ test.py
@@ -16,7 +16,6 @@ for i in range(chunkSize**2):
     block.x = floor(i/chunkSize*2)
     block.z = floor(i%chunkSize*2)
     block.y = floor(noise([block.x/freq, block.z/freq]) * amp)
-    #block.showTightBounds()
 
 
 class Player(Entity):
@@ -27,16 +26,21 @@ class Player(Entity):
         self.position = (0,6,0)
         self.scale=(0.4,0.4,0.4)
         self.collider = BoxCollider(self,(0,1.3,1),Vec3(self.model_bounds[0], self.model_bounds[1], self.model_bounds[2]-0.1))#custom box collider because original origin is slightly off
-        self.init_jump_energy = 2
+        self.init_jump_energy = 2.2
         self.jump_energy = 0
 
     def update(self):
-        print("p"+str(self.y))
+        if self.jump_energy > 0: 
+            self.y += self.jump_energy*0.1
+            if self.jump_energy > 0.1:
+                self.jump_energy -= 0.1
+            else:
+                self.jump_energy = 0
+    
         if not self.intersects().entities and not self.jump_energy > 0:#gravity
             self.y -= 0.2
-        if self.jump_energy > 0: 
-            self.y += self.jump_energy*0.2
-            self.jump_energy -= 0.2
+            for object_ in self.intersects().entities:
+                self.y = object_.y + 2.2
 
     def input(self,key):
         if key == 'tab':    # press tab to toggle edit/play mode
@@ -45,30 +49,29 @@ class Player(Entity):
         if held_keys["w"]:
             self.x += 0.08
             for object_ in self.intersects().entities:
-                if object_.y > self.y - 2.2:#check if the block is higher than the self's current y level
+                if object_.y > round(self.y) - 2:#check if the block is higher than the self's current y level
                     print(object_.y)
                     self.x -= 0.08
         elif held_keys["s"]:
             self.x -= 0.08
             for object_ in self.intersects().entities:
-                if object_.y > self.y - 2.2:#check if the block is higher than the self's current y level
+                if object_.y > round(self.y) - 2:#check if the block is higher than the self's current y level
                     print(object_.y)
                     self.x += 0.08
         elif held_keys["a"]:
             self.z += 0.08
             for object_ in self.intersects().entities:
-                if object_.y > self.y - 2.2:#check if the block is higher than the self's current y level
+                if object_.y > round(self.y) - 2:#check if the block is higher than the self's current y level
                     print(object_.y)
                     self.z -= 0.08
         elif held_keys["d"]:
             self.z -= 0.08
             for object_ in self.intersects().entities:
-                if object_.y > self.y - 2.2:#check if the block is higher than the self's current y level
+                if object_.y > round(self.y) - 2:#check if the block is higher than the self's current y level
                     print(object_.y)
                     self.z += 0.08
 
         if held_keys["space"]:
-            
             if self.intersects().entities:
                 self.jump_energy = self.init_jump_energy
 
